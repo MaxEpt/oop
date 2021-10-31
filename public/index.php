44ae97b436d4761107bc23993e900ed923d4966c
MaxEpt <mak_sim93@mail.ru>
2021-10-31 21:06:23 +0300

    ExampleService

--- public/index.php
+++ public/index.php
@@ -1,5 +1,16 @@
 <?
 require_once '../vendor/autoload.php';
+
+use App\BonusCard;
+use App\PromoCode;
+use App\ExampleService;
+
+
+$exampleService = new ExampleService(new BonusCard('123123123', 100), new PromoCode('1qwe12', 'PROMO', 'description'));
 ?>
 
-<h1>Hello, world</h1>
+<h1>EXAMPLE SERVICE RESULT</h1>
+<div>
+    <?= '<pre>'.print_r($exampleService->execute(), 1).'</pre>'; ?>
+</div>
+<hr />
--- src/ExampleService.php
+++ src/ExampleService.php
@@ -4,8 +4,27 @@ namespace App;
 
 class ExampleService
 {
-    public function run()
+    private $bonusCard;
+
+    private $promoCode;
+
+    public function __construct(BonusCard $bonusCard, PromoCode $promoCode)
     {
-        return 'hello, world';
+        $this->bonusCard = $bonusCard;
+        $this->promoCode = $promoCode;
+    }
+
+    public function execute()
+    {
+        return [
+            'BONUS_CARD' => [
+                'NUMBER' => $this->bonusCard->getNumber(),
+                'BALANCE' => $this->bonusCard->getBalance(),
+            ],
+            'PROMO_CODE' => [
+                'DISPLAY_VALUE' => $this->promoCode->getDisplayValue(),
+                'DESCRIPTION' => $this->promoCode->getDescription()
+            ],
+        ];   
     }
 }
